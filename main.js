d09e3866951854f00d70bba3db66556081bffbb3
Ben Yanick <byanick@ycp.edu>
2019-06-12 20:53:06 +0100

    Incorperated a small amount of Vue.js to organize image links more

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿new Vue({
+    el: "#app",
+
+    data: {
+        //C++ claculator desktop app image
+        CPPCalculator: "<img src='Img/Calculator.JPG' class='img-fluid mb-3' / >",
+
+        //C++ text editor desktop app image
+        CPPTextEditor: "<img src='Img/TextEditor.JPG' class='img-fluid mb-3' />",
+
+        //Arduino calculator image
+        electronicsCalculator: "<img src='Img/Calculator.jpeg' class='img-fluid mb-3' />",
+
+        //Rings Blender image
+        rings: "<img src='Img/RingCycles.png' class='img-fluid mb-3' / >",
+
+        //Traing Blender image
+        train: "<img src='Img/TrainCycles.jpg' class='img-fluid mb-3' / >",
+
+        //Boat Blender image
+        boat: "<img src='Img/BoatCycles.png' class='img-fluid mb-3' / >"
+
+    }
+})
