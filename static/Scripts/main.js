f89c6be97adf79bc4dc9353c5a5f261486c7f93b
Ben Yanick <byanick@ycp.edu>
2020-03-13 18:40:45 -0400

    Removed Bootstrap from website.  Currently only using vanilla CSS and a little bit of Vue.js.  Still need to add more styling to website to make it look appealing (BTY 03/13/20)

---
+++ static/Scripts/main.js
@@ -0,0 +1,33 @@
+﻿new Vue({
+    el: "#app",
+
+    data: {
+        //C++ claculator desktop app image
+        CPPCalculator: "<img src='static/Img/Index_Img/Calculator.JPG' class='img-fluid mb-3' / >",
+
+        //C++ text editor desktop app image
+        CPPTextEditor: "<img src='static/Img/Index_Img/TextEditor.JPG' class='img-fluid mb-3' />",
+
+        //Landing Page image
+        LandingPage: "<a href='Landing_Page.html'><img src='static/Img/Index_Img/Landing.JPG' /></a>",
+
+        //Python semantic AI for park reviews image
+        sentimentAI: "<img src='static/Img/Index_Img/ParkAI.JPG' class='img-fluid mb-3' />",
+
+        //Python minimax AI for tic tac toe image
+        ticTacToeAI: "<img src='static/Img/Index_Img/TicTacToeAI.JPG' class='img-fluid mb-3' />",
+
+        //Arduino calculator image
+        electronicsCalculator: "<img src='static/Img/Index_Img/Calculator.jpeg' class='img-fluid mb-3' />",
+
+        //Rings Blender image
+        rings: "<img src='static/Img/Index_Img/RingCycles.png' class='img-fluid mb-3' / >",
+
+        //Traing Blender image
+        train: "<img src='static/Img/Index_Img/TrainCycles.jpg' class='img-fluid mb-3' / >",
+
+        //Boat Blender image
+        boat: "<img src='static/Img/Index_Img/BoatCycles.png' class='img-fluid mb-3' / >"
+
+    }
+})
