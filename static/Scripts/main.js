3164f7a7258f3c7d1847277e4f44360c255fc267
Ben Yanick <byanick@ycp.edu>
2020-03-14 20:38:09 -0400

    Added more Blender projects to the website.  I am debating about adding in the electronics section now or later when I have a few more projects completed in that area of interest.  The last major thing I need to fix is the main landing point of the website (my site description) before I feel the website is ready to be redeployed, V1.02 of the website so to speak. (BTY 03/14/20)

--- static/Scripts/main.js
+++ static/Scripts/main.js
@@ -3,35 +3,65 @@
 
     data: {
 
-        //Picture of Myslef
+        //Picture of Myself
         selfImage: "<img src='static/Img/Index_Img/BenYanick.JPG' />",
 
+
+
+
+        // ***** PROGRAMMING IMAGES *****
+
         //C++ claculator desktop app image
-        CPPCalculator: "<img src='static/Img/Index_Img/Calculator.JPG' class='img-fluid mb-3' / >",
+        CPPCalculator: "<img src='static/Img/Index_Img/Programming/Calculator.JPG' class='img-fluid mb-3' / >",
 
         //C++ text editor desktop app image
-        CPPTextEditor: "<img src='static/Img/Index_Img/TextEditor.JPG' class='img-fluid mb-3' />",
+        CPPTextEditor: "<img src='static/Img/Index_Img/Programming/TextEditor.JPG' class='img-fluid mb-3' />",
+
+        
+
+        // ***** WEB DEV IMAGES *****
 
         //Landing Page image
         LandingPage: "<a href='Landing_Page.html'><img src='static/Img/Index_Img/Landing.JPG' /></a>",
 
+
+
+        // ***** AI IMAGES *****
+
         //Python semantic AI for park reviews image
-        sentimentAI: "<img src='static/Img/Index_Img/ParkAI.JPG' class='img-fluid mb-3' />",
+        sentimentAI: "<img src='static/Img/Index_Img/AI/ParkAI.JPG' class='img-fluid mb-3' />",
 
         //Python minimax AI for tic tac toe image
-        ticTacToeAI: "<img src='static/Img/Index_Img/TicTacToeAI.JPG' class='img-fluid mb-3' />",
+        ticTacToeAI: "<img src='static/Img/Index_Img/AI/TicTacToeAI.JPG' class='img-fluid mb-3' />",
+
+
+
+        // ***** ELECTRONICS IMAGES *****
 
         //Arduino calculator image
-        electronicsCalculator: "<img src='static/Img/Index_Img/Calculator.jpeg' class='img-fluid mb-3' />",
+        electronicsCalculator: "<img src='static/Img/Index_Img/Electronics/Calculator.jpeg' class='img-fluid mb-3' />",
+
+
+
+        // ***** BLENDER IMAGES ***** 
 
         //Rings Blender image
-        rings: "<img src='static/Img/Index_Img/RingCycles.png' class='img-fluid mb-3' / >",
+        rings: "<img src='static/Img/Index_Img/Blender/RingCycles.png' class='img-fluid mb-3' / >",
 
         //Traing Blender image
-        train: "<img src='static/Img/Index_Img/TrainCycles.jpg' class='img-fluid mb-3' / >",
+        train: "<img src='static/Img/Index_Img/Blender/TrainCycles.jpg' class='img-fluid mb-3' / >",
 
         //Boat Blender image
-        boat: "<img src='static/Img/Index_Img/BoatCycles.png' class='img-fluid mb-3' / >"
+        boat: "<img src='static/Img/Index_Img/Blender/BoatCycles.png' class='img-fluid mb-3' / >",
+
+        //Docks Blender image
+        docks: "<img src='static/Img/Index_Img/Blender/Dock_Improved_Eevee.png' />",
+
+        lego: "<img src='static/Img/Index_Img/Blender/Lego_Cycles_Bright.png' />",
+
+        vally: "<img src='static/Img/Index_Img/Blender/Vally.png' />",
+
+        hammer: "<img src='static/Img/Index_Img/Blender/Hammer_Eevee.png' />"
 
     }
 })
