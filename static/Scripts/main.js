66bf1415eaf28d8e3a6f55f0d9f6e5730f98af63
Ben3940 <bty2015@comcast.net>
2023-03-27 21:08:40 -0400

    Fixed issue on smaller screens where auto scroll would push the navbar over top of the section header

--- static/Scripts/main.js
+++ static/Scripts/main.js
@@ -2,7 +2,7 @@
 let sticky = navbar.offsetTop;
 
 window.onscroll = function () {
-  if (window.pageYOffset >= sticky) {
+  if (window.scrollY >= sticky) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
