8c7bf7d260396f8d06987199a4538332f32ad457
Ben3940 <bty2015@comcast.net>
2023-04-08 14:24:00 -0400

    Incorperated a large part of the grid layout.  Blender section and some minor font size and spacing si last thing required

--- static/Scripts/main.js
+++ static/Scripts/main.js
@@ -1,10 +1,11 @@
 ﻿let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
+let sticky = navbar.offsetHeight; //navbar.offsetTop;
 
-window.onscroll = function () {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
+// window.onscroll = function () {
+//   console.log(`Scroll Y: ${window.scrollY}  Sticky: ${sticky}`);
+//   if (window.scrollY !== 0) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// };
